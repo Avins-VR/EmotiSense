b8d226283db0372706866977babcbf29dd9d114c
Avins VR <avins2005@gmail.com>
2026-05-28 10:29:16 +0530

    Update emotion_model.py

--- emotion_model.py
+++ emotion_model.py
@@ -1,9 +1,6 @@
-import cv2
 from fer import FER
 
-# Load model
-detector = FER(mtcnn=True)
+detector = FER()
 
 def detect_emotions(frame):
-    results = detector.detect_emotions(frame)
-    return results
+    return detector.detect_emotions(frame)
